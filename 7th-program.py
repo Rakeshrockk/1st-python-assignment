38c15220f7cce652e816cff8d149bd3f688c0fb4
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2021-12-05 12:54:28 +0530

    till 7th updated

---
+++ 7th-program.py
@@ -0,0 +1,8 @@
+i=int(input("Enter the first 5 prime numbers :"))
+
+if(i==2 or i==3 or i==5 or i==7 or i==11):
+         print("You enterd",i,"and its is in range of first 5 prime numbers")
+         if(i==1):
+              print("Its is not a prime number by defination")
+         else:      
+             print("It is not in the range of first 5 prime numbers") 
