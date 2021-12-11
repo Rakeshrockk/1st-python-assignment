f25b38950f189f841e2831ccec0587f473c41205
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2021-12-12 00:03:03 +0530

    till slice updated

---
+++ loops and importfile/slice.py
@@ -0,0 +1,18 @@
+st="Hello world"
+print("st[0:5] is ", st[0:5]) 
+print("st[0:5:1] is", st[0:5:1]) 
+print("st[0:5] is ", st[0:5])
+print("st[3:8] is ", st[3:8])
+print("st[7:] is ", st[7:])
+print(st[::2])
+print("st[4:4] is ", st[4:4])
+print(st[3:8:2])
+print(st[1:8:3]) 
+print(st[-4:-1])
+print(st[-1:])
+print(st[:-1])
+print(st[::]) 
+print(st[::-1]) 
+print(st[::-2]) 
+
+
