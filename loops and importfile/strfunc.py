c4a75c56c3a0a945d7f69bff581592c8574a8670
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2021-12-11 18:47:31 +0530

    till strip updated

---
+++ loops and importfile/strfunc.py
@@ -0,0 +1,48 @@
+# capitalize(s)
+
+# s="hello" 
+# ss="World"
+# s1=str.capitalize(s)
+# s2=str.capitalize(ss)
+# print(s1+s2)
+
+# 2nd uppercase
+
+# s1=st.upper()
+# st="hello world"
+# print(s1)
+
+# 3rd lower case
+
+# s1="HELLO WORLD"
+# s2="hello woRld"
+# s3=s1.lower()
+# s4=s2.lower()
+# print(s3)
+# print(s4)
+
+# 4th s.find(s1)
+
+# st="hello world"
+# i=st.find('l')
+# print(i)
+# i2=st.find('lo')
+# print(i2)
+# i3=st.find(' ')
+# print(i3)
+# i4=st.find('d')
+# print(i4)
+# i5=st.find('x')
+# print(i5)
+
+# 5th st.strip function
+
+st5="      hello world        "
+s1=st5.strip()
+print(s1)
+st="##Hello world welcome to pyy##"
+s2=st.strip("#")
+print(s2)
+st2="hello world welcome to python"
+s3=st2.strip("ll")
+print(s3)
