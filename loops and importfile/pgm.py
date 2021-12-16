0979e9340d89ad66969ee5b29debe9277d0502b8
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2021-12-16 17:32:34 +0530

    str updated

--- loops and importfile/pgm.py
+++ loops and importfile/pgm.py
@@ -1,10 +1,17 @@
-def countChar(st,ch):
- count=0
- for i in st:
-    if i==ch:
-        count+=1
- return count
-st=input("Enter a string:")
-ch=input("Enter a character to be counted:")
-c=countChar(st,ch)
-print("{0} appeared {1} times in {2}".format(ch,c,st))
+# def countChar(st,ch):
+#  count=0
+#  for i in st:
+#     if i==ch:
+#         count+=1
+#  return count
+# st=input("Enter a string:")
+# ch=input("Enter a character to be counted:")
+# c=countChar(st,ch)
+# print("{0} appeared {1} times in {2}".format(ch,c,st))
+mystr="hello iam nkr and we are learning ..."
+print(mystr.upper())
+print(mystr.lower())
+print(mystr.capitalize())
+print(mystr.count("n"))
+print(mystr.find("n"))
+print(mystr.replace("nkr","Nithish"))
